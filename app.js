969e5b8ed6b6f32b5fd9e508cabbe01457acaa48
Rethash <rethashdevreddy@gmail.com>
2025-06-20 01:02:22 +0530

    user login, register and profile

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
+import session from "express-session"
+import bcrypt from "bcrypt"
+
 
 
 const app = express();
@@ -8,10 +11,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "YourKey",
+  resave: false,
+  saveUninitialized: false
+}));
+
+
+
 mongoose.connect(
   "mongodb://127.0.0.1:27017/blogDB", 
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
+
+const passSchema={
+  username: String,
+  password: String
+};
+
+const User = mongoose.model("User", passSchema);
 
 const postSchema = {
     title: String,
@@ -20,7 +38,53 @@ const postSchema = {
 
 const Post = mongoose.model("Post", postSchema);
 
+app.get("/register", (req, res)=>{
+  res.render("register");
+});
+
+app.get("/login", (req, res)=>{
+  res.render("login");
+});
+
+app.get("/profile", (req, res) => {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
+
+  res.render("profile", { user: req.session.user });
+});
+
+
+app.post("/register", async (req, res)=>{
+  const randPassword = await bcrypt.hash(req.body.password, 10);
+
+  const user = new User({
+    username: req.body.username,
+    password: randPassword
+  });
+
+  await user.save();
+  req.session.user=user;
+  res.redirect("/profile");
+});
+
+app.post("/login", async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+
+  if (user && await bcrypt.compare(req.body.password, user.password)) {
+    req.session.user = user;
+    res.redirect("/profile");
+  } else {
+    res.send("Invalid login");
+  }
+});
+
+
 app.get("/", async(req, res)=>{
+    if (!req.session.user) {
+    return res.redirect("/login");
+  }
+
     const posts= await Post.find();
     res.render("home", {posts: posts});
 });
